4a90340347f03ca2511bc1bcc7aa19078d82ef73
solo <cachecats@163.com>
2019-07-13 12:54:32 +0800

    project模块集成liquibase

---
+++ coderforum_project/project_web/src/main/java/com/coderiver/codeforum/project/config/LiquibaseConfig.java
@@ -0,0 +1,29 @@
+package com.coderiver.codeforum.project.config;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * @Author: solo
+ * @Date: 2019/7/11 10:33 PM
+ * @Version 1.0
+ */
+@Configuration
+public class LiquibaseConfig {
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource dataSource) throws Exception {
+
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource);
+        //指定changelog的位置，这里使用的一个master文件引用其他文件的方式
+        liquibase.setChangeLog("classpath:liquibase/master.xml");
+        liquibase.setContexts("development,test,production");
+        liquibase.setShouldRun(true);
+        return liquibase;
+    }
+}
